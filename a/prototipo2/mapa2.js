d53631740f0e0779bf1518e2cbf5a3ed266c7704
heitoryamamoto <heitoryamamoto426@gmail.com>
2023-05-11 00:19:09 -0300

    Update mapa2.js

--- a/prototipo2/mapa2.js
+++ a/prototipo2/mapa2.js
@@ -3,10 +3,10 @@ var y = 0
 var mapa = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 6, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 3, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
-    [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 7, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 3, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 7, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 1, 7, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 1],
+    [1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 0, 1, 1, 7, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 1],
     [1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 3, 0, 0, 0, 0, 1, 0, 0, 1, 1],
     [1, 0, 1, 0, 0, 0, 1, 4, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1],
     [1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 2, 1],
@@ -37,9 +37,8 @@ var bonecoL = 7
 var bonecoC = 7
 var chaveL = 11
 var chaveC = 10
-var tirarChave
 var vida = 0
-
+tmp = setInterval(movimento, 20)
 
 //1=Parede, 2=Porta Fechada, 3=Porta2 Fechada, 4=Chave1 ,5=Boneco, 6=Chave2
 
@@ -49,43 +48,43 @@ window.addEventListener("keydown", function andar(event) {
     if (tecla == "68") {
         if (mapa[bonecoL][bonecoC + 1] == 1 || mapa[bonecoL][bonecoC + 1] == 2) {
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[1][12]) {
             mapa[bonecoL][bonecoC] = 6
             bonecoC++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[2][27] && mapa[8][28] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoC++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[12][20] && mapa[8][20] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoC++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
-        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2){
+        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoC++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[9][11] && mapa[6][20] == 0) {
             mapa[bonecoL][bonecoC] = 5
             bonecoC++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else {
             mapa[bonecoL][bonecoC] = 0
             bonecoC++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
 
 
@@ -93,294 +92,206 @@ window.addEventListener("keydown", function andar(event) {
     if (tecla == "65") {
         if (mapa[bonecoL][bonecoC - 1] == 1 || mapa[bonecoL][bonecoC - 1] == 2) {
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[2][27] && mapa[8][28] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoC--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[5][14]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[5][13] = 7
             mapa[bonecoL][bonecoC] = 4
-            movimento()
-            vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
 
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
-            }
+            vida++
+            morte()
 
         }
         else if (mapa[bonecoL][bonecoC] == mapa[4][15]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[4][14] = 7
             mapa[bonecoL][bonecoC] = 4
-            movimento()
-            vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
 
-            }
+            vida++
+            morte()
 
         }
         else if (mapa[bonecoL][bonecoC] == mapa[3][16]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[3][15] = 7
+
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
             vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
-            }
+            morte()
         }
         else if (mapa[bonecoL][bonecoC] == mapa[2][17]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[2][16] = 7
-            mapa[bonecoL][bonecoC] = 4
-            movimento()
-            vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
 
-            }
+            mapa[bonecoL][bonecoC] = 4
+
+            vida++
+            morte()
         }
         else if (mapa[bonecoL][bonecoC] == mapa[12][20] && mapa[8][20] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoC--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
-        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2){
+        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoC--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[9][11] && mapa[6][20] == 0) {
             mapa[bonecoL][bonecoC] = 5
             bonecoC--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else {
             mapa[bonecoL][bonecoC] = 0
             bonecoC--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
     }
     if (tecla == "87") {
         if (mapa[bonecoL - 1][bonecoC] == 1 || mapa[bonecoL - 1][bonecoC] == 2) {
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[6][13]) {
             vida++
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[5][13] = 7
+
             mapa[bonecoL][bonecoC] = 4
-            movimento()
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
-            }
+
+            morte()
         }
         else if (mapa[bonecoL][bonecoC] == mapa[5][14]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[4][14] = 7
+
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
             vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
-            }
+            morte()
         }
         else if (mapa[bonecoL][bonecoC] == mapa[4][15]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[3][15] = 7
+
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
             vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
-            }
+            morte()
         }
         else if (mapa[bonecoL][bonecoC] == mapa[3][16]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[2][16] = 7
+
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
             vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
-            }
+            morte()
         }
         else if (mapa[bonecoL][bonecoC] == mapa[2][27] && mapa[8][28] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoL--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[8][28] && mapa[2][27] == 0) {
             mapa[bonecoL][bonecoC] = 5
             bonecoL--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[12][20] && mapa[8][20] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoL--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
-        else if(mapa[bonecoL][bonecoC] == mapa[8][20] && mapa[12][20] == 0){
+        else if (mapa[bonecoL][bonecoC] == mapa[8][20] && mapa[12][20] == 0) {
             mapa[bonecoL][bonecoC] = 5
             bonecoL--
             mapa[bonecoL][bonecoC] = 4
-            movimento() 
+
         }
-        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2){
+        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoL--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else {
             mapa[bonecoL][bonecoC] = 0
             bonecoL--
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
     }
     if (tecla == "83") {
         if (mapa[bonecoL + 1][bonecoC] == 1 || mapa[bonecoL + 1][bonecoC] == 2) {
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[1][16]) {
             mapa[bonecoL][bonecoC] = 0
             bonecoL = 7
             bonecoC = 7
-            mapa[2][16] = 7
+
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
             vida++
-            if (vida == 1) {
-                this.document.getElementById("vida3").src = ""
-
-            }
-            else if (vida == 2) {
-                this.document.getElementById("vida2").src = ""
-
-            }
-            else if (vida == 3) {
-                this.document.getElementById("vida1").src = ""
-                window.location.replace("gameover.html")
-            }
+            morte()
         }
 
         else if (mapa[bonecoL][bonecoC] == mapa[2][27] && mapa[8][28] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoL++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else if (mapa[bonecoL][bonecoC] == mapa[8][28] && mapa[2][27] == 0) {
             mapa[bonecoL][bonecoC] = 5
             bonecoL++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
-        else if(mapa[bonecoL][bonecoC] == mapa[8][20] && mapa[12][20] == 0){
+        else if (mapa[bonecoL][bonecoC] == mapa[8][20] && mapa[12][20] == 0) {
             mapa[bonecoL][bonecoC] = 5
             bonecoL++
             mapa[bonecoL][bonecoC] = 4
-            movimento() 
+
         }
-        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2){
+        else if (mapa[bonecoL][bonecoC] == mapa[6][20] && mapa[9][11] == 2) {
             mapa[bonecoL][bonecoC] = 3
             bonecoL++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
         else {
             mapa[bonecoL][bonecoC] = 0
             bonecoL++
             mapa[bonecoL][bonecoC] = 4
-            movimento()
+
         }
 
     }
@@ -658,4 +569,15 @@ function trap(params) {
         } document.getElementById("parede").innerHTML += "<br>"
     }
 }
-
+function morte(params) {
+    if (vida == 1) {
+        this.document.getElementById("vida3").src = ""
+    }
+    else if (vida == 2) {
+        this.document.getElementById("vida2").src = ""
+    }
+    else if (vida == 3) {
+        this.document.getElementById("vida1").src = ""
+        window.location.replace("gameover.html")
+    }
+}
