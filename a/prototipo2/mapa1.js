d2c3a0010a79787aca9497c1a4837d3e59a7cd87
heitoryamamoto <heitoryamamoto426@gmail.com>
2023-05-24 22:40:37 -0300

    Update mapa1.js

--- a/prototipo2/mapa1.js
+++ a/prototipo2/mapa1.js
@@ -136,16 +136,16 @@ function movimento() {
                 mapaAtualizacao += "* "
             }
             if (mapa[i][j] == 2) {
-                mapaAtualizacao += '<span style="color: yellow;">D </span>';
+                mapaAtualizacao += '<span style="color: yellow;text-shadow: 1px 1px 5px yellow">D </span>';
             }
             if (mapa[i][j] == 3) {
                 mapaAtualizacao += '<span style="color: deeppink; text-shadow: 1px 1px 5px deeppink">@ </span>';
             }
             if (mapa[i][j] == 4) {
-                mapaAtualizacao += '<span style="color: lime;text-shadow: 1px 1px 5px lime ">& </span>';
+                mapaAtualizacao += '<span style="color: lime;text-shadow: 1px 1px 5px lime">& </span>';
             }
             if (mapa[i][j] == 5) {
-                mapaAtualizacao += '<span style="color: orange;">= </span>';
+                mapaAtualizacao += '<span style="color: orange;text-shadow: 1px 1px 5px orange">= </span>';
             }
         } mapaAtualizacao += "<br>"
     }
